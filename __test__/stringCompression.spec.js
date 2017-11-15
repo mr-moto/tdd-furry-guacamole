85a0789342693b8a08f89bca86d88ac4680a7d7e
mr-moto <khash615@gmail.com>
2017-11-15 15:22:03 -0800

    added more tests

--- __test__/stringCompression.spec.js
+++ __test__/stringCompression.spec.js
@@ -1,9 +1,21 @@
 const stringCompression = require("../lib/stringCompression.js");
 describe("stringCompression", () => {
+        describe("when word is string", () => {
+        it("should return string of letters with numbers", () => {
+            const result = stringCompression('aaabbbccc');
+            expect(result).toEqual('a3b3c3');
+        });
+    });
     describe("when word is string", () => {
-        it("should return string of numnbers of letters", () => {
+        it("should return string of letters with numbers", () => {
             const result = stringCompression('ghjffj');
             expect(result).toEqual('g1h1j2f2');
         });
     });
+    describe("when string of emojis", () => {
+        it("should return string of emojis with numbers", () => {
+            const result = stringCompression('💩💩💩😎😎💩🍑🍑💩')
+            expect(result).toEqual('💩5😎2🍑2')
+        })
+    })
 });
