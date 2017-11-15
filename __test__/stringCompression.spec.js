f82367d8dac41cb2f22274f56302dfd326323aab
mr-moto <khash615@gmail.com>
2017-11-15 15:02:53 -0800

    added another way using reduce. works better

--- __test__/stringCompression.spec.js
+++ __test__/stringCompression.spec.js
@@ -2,8 +2,8 @@ const stringCompression = require("../lib/stringCompression.js");
 describe("stringCompression", () => {
     describe("when word is string", () => {
         it("should return string of numnbers of letters", () => {
-            const result = stringCompression('ghjff');
-            expect(result).toEqual('g1h1j1f2');
+            const result = stringCompression('ghjffj');
+            expect(result).toEqual('g1h1j2f2');
         });
     });
 });
